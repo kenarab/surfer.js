eb77ef1130ad78c366d32397b3a13e7ae5950bd9
ferpe <ferpe.email@gmail.com>
2014-06-29 16:06:44 -0300

    rendering with colors

--- src/org/test/server/GreetingServiceImpl.java
+++ src/org/test/server/GreetingServiceImpl.java
@@ -1,7 +1,11 @@
 
 package org.test.server;
 
+import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
+
+import javax.vecmath.Matrix4d;
 
 import org.apache.tools.ant.filters.StringInputStream;
 import org.test.client.GreetingService;
@@ -9,7 +13,12 @@ import org.test.shared.FieldVerifier;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
+import de.mfo.jsurf.rendering.AlgebraicSurfaceRenderer;
+import de.mfo.jsurf.rendering.JsAlgebraicSurfaceRenderer;
+import de.mfo.jsurf.rendering.LightSource;
 import de.mfo.jsurf.rendering.cpu.JsCPUAlgebraicSurfaceRenderer;
+import de.mfo.jsurf.util.BasicIO;
+import de.mfo.jsurf.util.FileFormat;
 
 /**
  * The server side implementation of the RPC service.
@@ -65,9 +74,9 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements Greetin
 
 	    if (equation == null || equation.trim().length() == 0)
 		equation = "(x^2+y^2+z^2+a^2-b^2)^2-4*b^2*(x^2+y^2)";
-	    
+
 	    JsCPUAlgebraicSurfaceRenderer renderer = new JsCPUAlgebraicSurfaceRenderer();
-//	    FileFormat.load(properties, renderer);
+	    load(properties, renderer);
 	    renderer.setSurfaceFamily(equation);
 	    return renderer;
 	}
@@ -76,4 +85,77 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements Greetin
 	    throw new RuntimeException(e);
 	}
     }
+
+    public static void load(Properties jsurf, JsAlgebraicSurfaceRenderer asr) throws Exception
+    {
+	asr.setSurfaceFamily(jsurf.getProperty("surface_equation"));
+
+	Set<Map.Entry<Object, Object>> entries = jsurf.entrySet();
+	String parameter_key_prefix = "surface_parameter_";
+	for (Map.Entry<Object, Object> entry : entries)
+	{
+	    String name = (String) entry.getKey();
+	    if (name.startsWith(parameter_key_prefix))
+	    {
+		String parameterName = name.substring(parameter_key_prefix.length());
+		asr.setParameterValue(parameterName, Float.parseFloat((String) entry.getValue()));
+	    }
+	}
+
+	asr.getCamera().loadProperties(jsurf, "camera_", "");
+	asr.getFrontMaterial().loadProperties(jsurf, "front_material_", "");
+	asr.getBackMaterial().loadProperties(jsurf, "back_material_", "");
+	for (int i = 0; i < AlgebraicSurfaceRenderer.MAX_LIGHTS; i++)
+	{
+	    asr.getLightSource(i).setStatus(LightSource.Status.OFF);
+	    asr.getLightSource(i).loadProperties(jsurf, "light_", "_" + i);
+	}
+	asr.setBackgroundColor(BasicIO.fromColor3fString(jsurf.getProperty("background_color")));
+
+	Matrix4d transform;
+	try
+	{
+	    transform = BasicIO.fromMatrix4dString(jsurf.getProperty("transform_matrix"));
+	}
+	catch (Exception e)
+	{
+	    // if the property isn't there initialize with identity matrix
+	    transform = new Matrix4d();
+	    transform.setIdentity();
+	}
+	try
+	{
+	    Matrix4d rotation_matrix = BasicIO.fromMatrix4dString(jsurf.getProperty("rotation_matrix"));
+	    transform.mul(rotation_matrix);
+	}
+	catch (Exception e)
+	{
+	}
+	asr.setTransform(transform);
+
+	Matrix4d surface_transform;
+	try
+	{
+	    surface_transform = BasicIO.fromMatrix4dString(jsurf.getProperty("surface_transform_matrix"));
+	}
+	catch (Exception e)
+	{
+	    // if the property isn't there initialize with identity matrix
+	    surface_transform = new Matrix4d();
+	    surface_transform.setIdentity();
+	}
+	try
+	{
+	    Matrix4d scale_matrix = new Matrix4d();
+	    scale_matrix.setIdentity();
+	    scale_matrix.setScale(Math.pow(10, Double.parseDouble(jsurf.getProperty("scale_factor"))));
+	    surface_transform.mul(scale_matrix);
+	}
+	catch (Exception e)
+	{
+	}
+
+	asr.setSurfaceTransform(surface_transform);
+    }
+
 }
